708ef3dd7e5309b287cbcac4e3adbded63b58574
sethirajendra15@gmail.com <sethirajendra15@gmail.com>
2025-12-03 03:25:45 +0000

    new changes

---
+++ snapshots/orders_cdc.sql
@@ -0,0 +1,31 @@
+{% snapshot supply_chain_orders_snapshot_key_based_hard_delete %}
+{{
+    config(
+    database="DBT_DB2",
+    target_schema="SILVER_SCH",
+    unique_key="ORDER_ID",
+    strategy="check",
+    check_cols=["SUPPLIER_NAME","PRODUCT_NAME","QUANTITY","UNIT_COST","TOTAL_COST","STATUS","DELIVERY_DATE"],
+    snapshot_freshness='7 days',
+    query_tag='dbt'
+    )
+}}
+
+select
+        order_id,
+        supplier_id,
+        supplier_name,
+        product_id,
+        product_name,
+        warehouse_id,
+        warehouse_region,
+        order_date,
+        delivery_date,
+        quantity,
+        unit_cost,
+        total_cost,
+        status,
+        updated_at
+    from {{ source("raw_cust", "SUPPLY_CHAIN_ORDERS") }}
+
+{% endsnapshot %}
